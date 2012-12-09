0158bd00c58b7af8d5a086b172bc859cbd050a1e
Danilo Raniery <danilo.coutinho@dce.ufpb.br>
2012-12-08 23:50:13 -0200

    Exercício 9
    Classes AgendaTeste e AgendaMapTeste adicionadas

---
+++ Exercício 9/TestaAgenda.java
@@ -0,0 +1,56 @@
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.*;
+
+public class TestaAgenda {
+	
+		
+		@Test
+		public void AgendaTest() {
+			Agenda agenda = new Agenda();
+			
+			try{
+				agenda.adicionarContato("Danilo", "111");
+			}catch (ContatoExistenteException exc){
+				fail("O contato Danilo não foi adicinado.");
+			}
+			
+			try{
+				assertEquals(agenda.pesquisarContato("Danilo").getNome(), "Danilo");
+			} catch  (ContatoInexistenteException exc){
+				fail("O contato Danilo não foi encontrado.");
+			}
+						
+			try{
+				agenda.removerContato("Danilo");
+			} catch (ContatoInexistenteException excecao1){
+				fail("O contato Danilo não foi encontrado para ser removido.");
+			
+			}
+			
+			try{
+				assertEquals(agenda.pesquisarContato("Danilo").getNome(), "Danilo");
+				fail("Removeu um contato que não existe.");
+			} catch  (ContatoInexistenteException excecao){		
+			}
+			
+			try{
+				agenda.adicionarContato("Danilo", "1111");
+				agenda.adicionarContato("Ayla", "2222");
+			} catch (ContatoExistenteException excecao){
+				fail("Erro ao adicionar os contatos Ayla e Danilo");
+			}
+			
+			Iterator <Contato> contOrdenados = agenda.getContatosOrdenados();
+			assertTrue(contOrdenados.hasNext());
+			
+			Contato cont1 = contOrdenados.next();
+			assertEquals("Danilo", cont1.getNome());
+			
+			assertTrue(contOrdenados.hasNext());
+			Contato cont2 = contOrdenados.next();
+			assertEquals("Ayla", cont2.getNome());
+		}
+}
+
